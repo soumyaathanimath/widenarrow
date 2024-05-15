93cbbf9e6a077d9346738da8914514ef03892118
soumyaaaaaaaaaaaaaaaaaaaaaaa <167617017+soumyaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-05-15 11:33:44 +0530

    widening narrwoing

---
+++ src/wide/Wide.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package wide;
+
+/**
+ *
+ * @author dell1
+ */
+public class Wide {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     
+	
+        
+		byte b=10;
+		short s=10;
+		int i=10;
+		long l=10;
+		float f=10;
+		double d=10;
+		char c=10;
+		boolean bl=true;
+
+        }
+}
+        
+
+
+
+ 
